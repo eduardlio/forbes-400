6659a0bdae82b0fe0613b69bac986a2cc8c366a8
eduardlio <liao.edward@gmail.com>
2017-12-13 20:00:05 +1000

    it's like, functioning now i guess

---
+++ index.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    var people = [];
+    var currentPerson;
+    var max;
+    $.get("people.json", function (data) {
+        data.people.forEach((person, i)=>{
+            people.push(person);
+        });
+        max = people.length;
+        showRandomRichPerson();
+    })
+    $("#random").on("click", ()=>{
+        showRandomRichPerson();
+    })
+    function showRandomRichPerson(){
+        var random = Math.floor(Math.random()*max);
+        currentPerson = people[random];
+        $("#person").html(currentPerson.name);
+        $("#worth").html(currentPerson.net.toLowerCase());
+        $("#score").html(currentPerson.score);
+        $("#source").html(currentPerson.source);
+        $("#age").html(currentPerson.age);
+        var education = 
+        currentPerson.education.trim().length > 0
+        ? currentPerson.education
+        : "N/A";
+        $("#education").html(education);
+        var description = 
+        currentPerson.description.trim().length > 0
+        ? currentPerson.description
+        : "N/A";
+        $("#desc").html(description);
+    }
+})
