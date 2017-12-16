212072b708bd70c0ff8cb3586f42521e958f3c2d
eduardlio <liao.edward@gmail.com>
2017-12-16 17:48:44 +1000

    Made more mobile friendly, added next and prev buttons

--- index.js
+++ index.js
@@ -1,34 +1,66 @@
 $(document).ready(function () {
-    var people = [];
-    var currentPerson;
-    var max;
-    $.get("people.json", function (data) {
-        data.people.forEach((person, i)=>{
-            people.push(person);
-        });
-        max = people.length;
-        showRandomRichPerson();
-    })
-    $("#random").on("click", ()=>{
-        showRandomRichPerson();
-    })
-    function showRandomRichPerson(){
-        var random = Math.floor(Math.random()*max);
-        currentPerson = people[random];
-        $("#person").html(currentPerson.name);
-        $("#worth").html(currentPerson.net.toLowerCase());
-        $("#score").html(currentPerson.score);
-        $("#source").html(currentPerson.source);
-        $("#age").html(currentPerson.age);
-        var education = 
-        currentPerson.education.length > 0
-        ? currentPerson.education
-        : "N/A";
-        $("#education").html(education);
-        var description = 
-        currentPerson.description.length > 0
-        ? currentPerson.description
-        : "N/A";
-        $("#desc").html(description);
-    }
+   var people = [];
+   var currentPerson;
+   var max;
+   var index = 0;
+   $.get("people.json", function (data) {
+      data.people.forEach((person, i)=>{
+         people.push(person);
+      });
+      max = people.length;
+      showRandomPerson();
+   })
+   $("#random").on("click", ()=>{
+      showRandomPerson();
+   })
+   $("#prev").on("click", ()=>{
+      showPrevPerson();
+   });
+   $("#next").on("click", ()=>{
+      showNextPerson();
+   });
+   function logIndex(){
+      console.log(index);
+   }
+   function showNextPerson(){
+      index = index >= max 
+         ? 0 
+         : index+1;
+      logIndex();
+      currentPerson = people[index];
+      showPerson();
+   }
+   function showPrevPerson(){
+      index = index < 0 
+         ? max-1 
+         : index+1;
+      logIndex();
+      currentPerson = people[index];
+      showPerson();
+   } 
+   function showRandomPerson(){
+      var random = Math.floor(Math.random()*max);
+      index = random;
+      currentPerson = people[index];
+      logIndex();
+      showPerson();
+   }
+   function showPerson(){
+      $("#wrap").attr("id", index);
+      $("#person").html(currentPerson.name);
+      $("#worth").html(currentPerson.net.toLowerCase());
+      $("#score").html(currentPerson.score);
+      $("#source").html(currentPerson.source);
+      $("#age").html(currentPerson.age);
+
+      var education = currentPerson.education.length > 0
+         ? currentPerson.education
+         : "N/A";
+      $("#education").html(education);
+
+      var description = currentPerson.description.length > 0
+         ? currentPerson.description
+         : "N/A";
+      $("#desc").html(description);
+   }
 })
